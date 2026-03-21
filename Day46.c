2faef761aaa830b77860977d3aa19f02fcaf48ac
Manish071167 <manishmbudhwar@gmail.com>
2026-03-21 02:40:49 +0000

    Day 46: Implemented Queue Operations (Enqueue/Dequeue)

--- Day46.c
+++ Day46.c
@@ -1,1 +1,12 @@
-// Day 46: Data Structures and Algorithms Practice
+#include <stdio.h>
+#include <stdlib.h>
+
+// Level Order Traversal implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
