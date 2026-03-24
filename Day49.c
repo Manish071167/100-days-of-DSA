56360622188eb7520519ef0e676169693db3ab29
Manish071167 <manishmbudhwar@gmail.com>
2026-03-24 22:56:39 +0000

    Day 49: Implemented Queue Operations (Enqueue/Dequeue)

--- Day49.c
+++ Day49.c
@@ -1,1 +1,12 @@
-// Day 49: Data Structures and Algorithms Practice
+#include <stdio.h>
+#include <stdlib.h>
+
+// BST Insert implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
