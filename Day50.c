67c42e887efafc8f87ce5387ffc391b50f5d10bb
Manish071167 <manishmbudhwar@gmail.com>
2026-03-25 19:34:11 +0000

    Day 50: Implemented Queue Operations (Enqueue/Dequeue)

--- Day50.c
+++ Day50.c
@@ -1,1 +1,12 @@
-// Day 50: Data Structures and Algorithms Practice
+#include <stdio.h>
+#include <stdlib.h>
+
+// BST Search implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
