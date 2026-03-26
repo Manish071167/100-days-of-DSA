cc36980fe063081fe9b2a5c1a789a9b695671cfb
Manish071167 <manishmbudhwar@gmail.com>
2026-03-26 09:21:09 +0000

    Day 51: Implemented Queue Operations (Enqueue/Dequeue)

--- Day51.c
+++ Day51.c
@@ -1,1 +1,11 @@
-// Day 51: Data Structures and Algorithms Practice
+struct Node* LCA(struct Node* root, int n1, int n2) {
+    while(root != NULL) {
+        if(n1 < root->data && n2 < root->data)
+            root = root->left;
+        else if(n1 > root->data && n2 > root->data)
+            root = root->right;
+        else
+            return root;
+    }
+    return NULL;
+}
