886ea81e88e184874daf6254600674aa801cae3d
Manish071167 <manishmbudhwar@gmail.com>
2026-04-07 20:14:01 +0000

    Day 61: Implemented Queue Operations (Enqueue/Dequeue)

--- Day61.c
+++ Day61.c
@@ -1,1 +1,16 @@
-// Day 61: Data Structures and Algorithms Practice
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> adj(n + 1, vector<int>(n + 1, 0));
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = 1;
+        adj[v][u] = 1; 
+    }
+    return 0;
+}
