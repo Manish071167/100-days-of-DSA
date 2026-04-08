86bb127941984c7e7afc51f29f6fa0e6464f612e
Manish071167 <manishmbudhwar@gmail.com>
2026-04-08 23:47:19 +0000

    Day 62: Implemented Queue Operations (Enqueue/Dequeue)

--- Day62.c
+++ Day62.c
@@ -1,1 +1,16 @@
-// Day 62: Data Structures and Algorithms Practice
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> adj[n + 1];
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u); 
+    }
+    return 0;
+}
