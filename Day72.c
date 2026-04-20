b2acd5ec1fc778cb2722831f0c07a19dd178a3c4
Manish071167 <manishmbudhwar@gmail.com>
2026-04-20 23:04:56 +0000

    Day 72

--- Day72.c
+++ Day72.c
@@ -1,1 +1,26 @@
-// Day 72: Data Structures and Algorithms Practice
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    int freq[26] = {0};
+    int found = 0;
+
+    scanf("%s", s);
+
+    for(int i = 0; s[i] != '\0'; i++) {
+        int idx = s[i] - 'a';
+        freq[idx]++;
+
+        if(freq[idx] == 2) {
+            printf("%c", s[i]);
+            found = 1;
+            break;
+        }
+    }
+
+    if(!found)
+        printf("-1");
+
+    return 0;
+}
